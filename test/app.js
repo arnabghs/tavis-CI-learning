77d9c5a731f4ec1d2f16e2b2465b36de881014f6
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-02-14 15:22:20 +0530

    initial commit

---
+++ test/app.js
@@ -0,0 +1,9 @@
+const add = function(a, b) {
+  return a + b;
+};
+
+const sub = function(a, b) {
+  return a - b;
+};
+
+module.exports = { add, sub };
---
+++ test/integration_test.js
@@ -0,0 +1,28 @@
+const assert = require("assert");
+const { add, sub } = require("../app.js");
+
+describe("add", () => {
+  it("pass test", () => {
+    let actualOutput = add(3, 3);
+    let expectedOutput = 6;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+  it("pass test", () => {
+    let actualOutput = add(5, 5);
+    let expectedOutput = 10;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
+
+describe("sub", () => {
+  it("pass test", () => {
+    let actualOutput = sub(3, 1);
+    let expectedOutput = 2;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+  it("pass test", () => {
+    let actualOutput = sub(10, 2);
+    let expectedOutput = 8;
+    assert.deepEqual(actualOutput, expectedOutput);
+  });
+});
