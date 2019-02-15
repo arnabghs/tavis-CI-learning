65c914e1d6a49397d28d93b4adcf2f58d4fd2e05
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-02-15 18:04:47 +0530

    added coverage tool. all test should pass.

--- test/unit_test.js
+++ test/unit_test.js
@@ -9,7 +9,7 @@ describe("add", () => {
   });
   it("fail test", () => {
     let actualOutput = add(5, 5);
-    let expectedOutput = 100;
+    let expectedOutput = 10;
     assert.deepEqual(actualOutput, expectedOutput);
   });
 });
